800c965952b2ffa80d1da884eb6265c0d7533d3a
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2021-04-09 23:28:31 +0200

    Updated setup descr.

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from distutils.core import setup
 from setuptools import find_packages
+import sys
+
+from pathlib import Path  # noqa E402
+
+CURRENT_DIR = Path(__file__).parent
+sys.path.insert(0, str(CURRENT_DIR))
+
+
+def get_long_description() -> str:
+    return (
+        (CURRENT_DIR / "README.md").read_text(encoding="utf8")
+    )
+
 
 setup(
     name="matrixreg",
@@ -7,6 +20,8 @@ setup(
     version="0.1",
     license="MIT",
     description="Implementation of the MatrixRegression (MR) algorithm for online-learning multi-label text classification, by Popa, Zeitouni & Gardarin",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     author="Nicolò Verardo",
     author_email="n.verardo@outlook.com",
     url="https://github.com/nicoloverardo/matrix_regression",
@@ -20,7 +35,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
